074e6baa96dfb57d91af78d2a800ab2d1799a05b
Kevin Osborn <kevin.osborn@gmail.com>
2021-04-03 18:11:31 -0400

    add interrupt handling to plotrides

--- plotrides.py
+++ plotrides.py
@@ -1,9 +1,19 @@
 import sys
+import signal
 import os
 from numpy import *
 import matplotlib.pyplot as plt
 import datetime
+import time
 
+def sig(a,b):
+    print("got sigint, exiting")
+    os._exit(0)
+
+def timercb(e):
+    print("tick")
+    
+signal.signal(signal.SIGINT,sig)
 from peloton import PelotonWorkout
 workouts = PelotonWorkout.list()
 #grab the first cycling workout, other types don't have resistance
@@ -14,7 +24,8 @@ for workout in workouts:
         output = metrics.output
         heart_rate= metrics.heart_rate
         cadence = metrics.cadence
-        plt.figure(1,figsize = (9,9))
+        fig = plt.figure(1,figsize = (9,9))
+        ax = plt.gca()
         plt.suptitle("%s\n %s" % (workout.ride.title,workout.start_time.strftime("%m/%d.%Y")))
         plt.subplot(411)
         plt.ylabel("resistance")
@@ -28,6 +39,12 @@ for workout in workouts:
         plt.subplot(414)
         plt.ylabel("heart rate")
         plt.plot(heart_rate.values)
-        plt.show()
+        timer = fig.canvas.new_timer(interval=5000)
+        timer.add_callback(timercb,ax)
+        timer.start()
+        try:
+            plt.show()
+        except KeyboardInterrupt:
+            sys.exit(0)
     
     
