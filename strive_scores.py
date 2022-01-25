68a35fc5341bf9e43c895888f441d6e51b79bf43
Kevin Osborn <kevin.osborn@gmail.com>
2022-01-24 19:05:57 -0500

    print out bucket scores nicely

--- strive_scores.py
+++ strive_scores.py
@@ -21,12 +21,13 @@ for workout in workouts:
             duration = workout.ride.duration/60
             ride_length = "%d" %duration
             strive_score = workout.strive_score
-            workout_title ="%s on %s" % (workout.ride.title,workout.start_time.strftime("%m/%d/%Y"))
-            print(workout_title)
-            print("strive_score: %0.1f" %strive_score)
+            workout_title ="%s with %s on %s" % (workout.ride.title,workout.ride.instructor,workout.start_time.strftime("%m/%d/%Y"))
+#            print(workout_title)
+#            print("strive_score: %0.1f" %strive_score)
             if strive_score > records[ride_length]['Strive']:
                 records[ride_length]['Class'] = workout_title
                 records[ride_length]['Strive'] = strive_score
-print(records)
+for time_workout in records:
+    print("%s Minute Record: %s, %.1f" % (time_workout, records[time_workout]['Class'], records[time_workout]['Strive']))
     
     
