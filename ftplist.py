645b7cd26bbd7462c45b96a2dd12569213c6b463
osbock <kevin.osborn@gmail.com>
2025-01-23 15:38:24 -0500

    change ftplist to csv format

--- ftplist.py
+++ ftplist.py
@@ -1,9 +1,10 @@
 from peloton import PelotonWorkout
 workouts = PelotonWorkout.list()
+print("Date,FTP Score (kj),Max Heartrate")
 for workout in workouts:
     if workout.ride.title == '20 min FTP Test Ride':
         metrics = workout.metrics
-        print("Date: %s FTP Score: %0.2f %s, max_heartrate %d" % (workout.created.strftime("%m-%d-%Y"),metrics.output.average *.95,metrics.output_summary.unit, metrics.heart_rate.max))
+        print("%s, %0.2f, %d" % (workout.created.strftime("%m-%d-%Y"),metrics.output.average *.95, metrics.heart_rate.max))
         
 
     
